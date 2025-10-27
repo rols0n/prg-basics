348c7b58333b33a97caafac3f67d6f5e3f6d59ac
Patryk Rolka <rolson.priv@gmail.com>
2025-10-27 12:53:35 +0100

    done 2-4

---
+++ 02-TypesAndVariables/2-3.py
@@ -0,0 +1,74 @@
+# ## 3
+
+# ###
+# # A program for swapping two varable values
+# #
+# x = 7
+# y = 34
+# z = 0 # additional, auxiliary variable
+# print("Before swapping: x=", x, "y=", y)
+
+# # swap the values
+
+# x = 34;
+# y = 7;
+
+# print("After swapping: x=", x, "y=", y)
+
+## 4
+###
+# A program that, for a given speed in km/h,
+# prints the speed in m/s
+#
+# speed_kmh = 30
+# speed_ms = speed_kmh * 1000/3600;
+# print(speed_kmh, "km/h = ", speed_ms, "m/s")
+
+###
+# A program that calculates the length of the diagonal
+# of a rectangle with sides a and b.
+# 5
+# import math
+# a = 5;
+# b = 8;
+# sum = a**2 + b**2;
+# diagonal = math.sqrt(sum);
+# print(diagonal)
+
+### 6
+
+# d = 3.57 × √h
+# import math;
+
+# h = int(input("Give the height of your horizon: "));
+
+# d = 3.57 * math.sqrt(h);
+# print(d);
+
+
+###
+# A program that calculates and prints:
+# - the number of people and percentage of the total
+#   population living in the Northern Hemisphere
+# - the number of people and percentage of the total
+#   population living in the Southern Hemisphere
+#
+# total = 8000000000
+# north = 7200000000
+# south = total - north;
+# print("World population: ", total)
+# print("Northern Hemisphere: ", north)
+# print("Northern Hemisphere in %: ", north/total*100)
+# print("Southern Hemisphere: ", south)
+# print("Southern Hemisphere in %: ", south/total*100)
+
+###
+# A program that calculates and prints
+# the average grade of a student
+#
+# math = 5
+# art = 4
+# music = 5
+# history = 3
+# average = (math + art + music + history) / 4
+# print('Average grade is', average);
